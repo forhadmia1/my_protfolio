0a1c2e515f8367505915f84a45d4498a55597a2d
forhadmia1 <forhad8613@gmail.com>
2022-06-10 02:56:13 +0600

    hero area section add animation

--- src/pages/Home/HeroArea.js
+++ src/pages/Home/HeroArea.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { saveAs } from "file-saver";
 import heroImg from '../../Assets/Image/IMG20190904153853-01.png'
+import '../../App.css';
 
 const HeroArea = () => {
     const saveFile = () => {
@@ -10,21 +11,23 @@ const HeroArea = () => {
         );
     };
     return (
-        <section className='grid grid-cols-1 md:grid-cols-2 mt-10'>
-            <div>
-                <img className='w-full md:h-[80vh]' src={heroImg} alt="Forhad hasan Pic" />
-            </div>
-            <div className='flex items-center md:ml-[-80px] md:w-[680px] w-full'>
-                <div className='bg-slate-800 py-16 md:px-20 px-4'>
-                    <h2 className='text-4xl font-bold mb-4 text-white'>Forhad Mia</h2>
-                    <h3 className='mb-4 text-2xl'>I am a web devloper</h3>
+        <section className='grid grid-cols-1 md:grid-cols-2 py-20 px-12 mt-10 justify-items-center place-items-center'>
+            <div className='flex items-center'>
+                <div>
+                    <p className='text-xl mb-2'>Hello, I am</p>
+                    <h2 className='text-6xl font-bold mb-4 text-white'>Forhad Mia</h2>
+                    <h3 className='mb-4 text-2xl banner-title font-semibold'>I am a</h3>
                     <p className='mb-4'>
                         I am very passionate about working with programming and related tasks. I am very curious to explore new technologies. I am hardworking and a reliable person.
                     </p>
                     <button class="btn btn-outline btn-secondary px-12 rounded-3xl border-2  " onClick={saveFile}>Download Resume</button>
                 </div>
             </div>
+            <div className='p-3 rounded-full bg-slate-400 w-fit'>
+                <img className='w-80 h-80 rounded-full' src={heroImg} alt="" />
+            </div>
         </section>
+
     );
 };
 
