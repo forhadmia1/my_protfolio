4a461af6ce39f8f4184d7d41ad502b9dbe74950a
forhadmia1 <forhad8613@gmail.com>
2022-06-22 15:31:48 +0600

    navbar dropdown take to right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,20 +6,22 @@ const Navbar = () => {
         <nav className='bg-base-100 md:px-12 sticky top-0 z-50'>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
+                    <Link to='/' className="md:pl-0 text-primary font-bold btn btn-ghost normal-case text-4xl">Forhad.</Link>
+                </div>
+                <div className='w-full justify-end'>
                     <div className="dropdown">
                         <label tabIndex="0" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/home'>Home</Link></li>
-                            <li><Link to='/blogs'>Blogs</Link></li>
-                            <li><Link to='/projects'>Projects</Link></li>
-                            <li><Link to='/about'>About</Link></li>
-                            <li><Link to='/contact'>Contact</Link></li>
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-48 right-1">
+                            <li className='items-end'><Link className='w-full justify-end' to='/home'><span>Home</span></Link></li>
+                            <li className='items-end'><Link className='w-full justify-end' to='/blogs'><span>Blogs</span></Link></li>
+                            <li className='items-end'><Link className='w-full justify-end' to='/projects'><span>Projects</span></Link></li>
+                            <li className='items-end'><Link className='w-full justify-end' to='/about'><span>About</span></Link></li>
+                            <li className='items-end'><Link className='w-full justify-end' to='/contact'><span>Contact</span></Link></li>
                             <li><a target='_blank' rel="noreferrer" href='https://drive.google.com/file/d/15ka36LpmP1yD4NX7_7Iw7iydKCtjP3fa/view?usp=sharing' className="btn bg-primary hover:bg-dark hover:text-white border-none px-4 font-semibold" download>Resume</a></li>
                         </ul>
                     </div>
-                    <Link to='/' className="text-primary font-bold btn btn-ghost normal-case text-4xl">Forhad.</Link>
                 </div>
                 <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal p-0 font-semibold">
