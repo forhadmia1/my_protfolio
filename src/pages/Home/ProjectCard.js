24f0b184734fa838b35d8790498e9881dec6c8f5
forhadmia1 <forhad8613@gmail.com>
2022-06-23 02:36:18 +0600

    animation issue fixed

--- src/pages/Home/ProjectCard.js
+++ src/pages/Home/ProjectCard.js
@@ -12,14 +12,16 @@ const ProjectCard = ({ project }) => {
     }, []);
 
     return (
-        <div data-aos="zoom-in" className="card w-full bg-base-100 shadow-2xl hover:-translate-y-5 duration-300">
-            <div className='p-4'>
-                <figure><img className='rounded-lg hover:scale-110 duration-300' src={project?.image[0]} alt={project?.name} /></figure>
-            </div>
-            <div className='px-4 pb-4'>
-                <h2 className="card-title">{project?.name}</h2>
-                <div className="card-actions justify-start mt-4 w-full">
-                    <button onClick={() => navigate(`/project/${project?.id}`)} className="btn w-full hover:text-white rounded-none btn-primary">Show Details</button>
+        <div data-aos="zoom-in">
+            <div className="card w-full bg-base-100 shadow-2xl hover:-translate-y-5 duration-300">
+                <div className='p-4'>
+                    <figure><img className='rounded-lg' src={project?.image[0]} alt={project?.name} /></figure>
+                </div>
+                <div className='px-4 pb-4'>
+                    <h2 className="card-title">{project?.name}</h2>
+                    <div className="card-actions justify-start mt-4 w-full">
+                        <button onClick={() => navigate(`/project/${project?.id}`)} className="btn w-full hover:text-white rounded-none btn-primary">Show Details</button>
+                    </div>
                 </div>
             </div>
         </div>
