553dbc0ebd48faf3b738cade878eda35254858d8
forhadmia1 <forhad8613@gmail.com>
2022-09-15 14:30:44 +0600

    error page done

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import error from '../Assets/Image/error.jpg'
+import { useNavigate } from 'react-router-dom';
+import Footer from '../components/common/Footer';
 
 const NotFound = () => {
+    const navigate = useNavigate()
     return (
-        <div className='h-[90vh]'>
-            <img className='h-full w-full' src={error} alt="" />
+        <div className='h-screen'>
+            <div className='h-[67%] flex justify-center items-center flex-col'>
+                <p className='text-3xl text-neutral'>Page not found!!</p>
+                <button onClick={() => navigate('/')} className="p-3 border-2 border-primary rounded text-secondary mt-5">Back To Home</button>
+            </div>
+            <Footer />
         </div>
     );
 };
